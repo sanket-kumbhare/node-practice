92d204259e10b2de2da137e9e8ca694c8c1f29f7
sanket-kumbhare <sanketkumbhare1511@gmail.com>
2023-11-05 00:39:01 +0530

    https example

---
+++ http-server/index.js
@@ -0,0 +1,47 @@
+const http = require("http");
+
+const server = http.createServer();
+
+const friends = [
+  { id: 0, name: "Taylor Otwell" },
+  { id: 1, name: "Ryan Dahl" },
+  { id: 2, name: "Jordan Walke" },
+];
+
+server.on("request", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const url = req.url.split("/");
+  if (req.method == "POST" && url[1] == "friends") {
+    req.on("data", (chunks) => {
+      let friend = chunks.toString();
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+    return;
+  } else if (
+    url[1] == "friends" &&
+    req.method == "GET" &&
+    url.length == 3 &&
+    url[2] <= 2
+  ) {
+    res.setHeader("status", 200);
+    const id = Number(url[2]);
+    res.write(JSON.stringify(friends[id]));
+  } else if (
+    url[1] == "friends" &&
+    req.method == "GET" &&
+    url.length == 3 &&
+    url[2] > 2
+  ) {
+    res.setHeader("status", 404);
+    res.write("User not found");
+  } else if (url[1] == "friends") {
+    res.setHeader("status", 200);
+    res.write(JSON.stringify(friends));
+  } else {
+    res.write("404 page not found");
+  }
+  res.end();
+});
+
+server.listen(4001);
--- space/index.js
+++ space/index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const result = [];
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("./kepler_data.csv")
   .pipe(
@@ -11,11 +20,18 @@ fs.createReadStream("./kepler_data.csv")
     })
   )
   .on("data", (chunk) => {
-    result.push(chunk);
+    if (isHabitable(chunk)) {
+      habitablePlanets.push(chunk);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(result);
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    // console.log(habitablePlanets);
   });
